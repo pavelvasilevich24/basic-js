4b056336d34dfe4183fe375ab069d13de21b110f
pavelvasilevich24 <pavelvasilevich24@gmail.com>
2025-01-02 23:31:33 +0300

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const input = message.toUpperCase();
+    const keyword = key.toUpperCase();
+
+    let result = [];
+    let keyIndex = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      if (this.alphabet.includes(char)) {
+        const charIndex = this.alphabet.indexOf(char);
+        const keyCharIndex = this.alphabet.indexOf(keyword[keyIndex % keyword.length]);
+        const encryptedCharIndex = (charIndex + keyCharIndex) % this.alphabet.length;
+
+        result.push(this.alphabet[encryptedCharIndex]);
+        keyIndex++;
+      } else {
+        result.push(char);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const input = message.toUpperCase();
+    const keyword = key.toUpperCase();
+    let result = [];
+    let keyIndex = 0;
+
+    for (let j = 0; j < input.length; j++) {
+      const char = input[j];
+      if (this.alphabet.includes(char)) {
+        const charIndex = this.alphabet.indexOf(char);
+        const keyCharIndex = this.alphabet.indexOf(keyword[keyIndex % key.length]);
+        const decryptedCharIndex = (charIndex - keyCharIndex + this.alphabet.length) % this.alphabet.length;
+        result.push(this.alphabet[decryptedCharIndex]);
+        keyIndex++;
+      } else {
+        result.push(char);
+      }
+    }
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
