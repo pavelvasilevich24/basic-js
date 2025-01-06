5085dc3949226ffb5d2e7f5ec478c54cc71e5b5a
pavelvasilevich24 <pavelvasilevich24@gmail.com>
2025-01-07 00:50:13 +0300

    feat: dns-stats.js

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const re = {};
+
+  domains.forEach((item) => {
+    const domains = item.split(".").reverse();
+    let domain = "";
+    domains.forEach((el) => {
+      domain += `.${el}`;
+      if (res.hasOwnProperty(domain)) {
+        res[domain] += 1;
+      } else {
+        res[domain] = 1;
+      }
+    })
+  })
+
+  return res;
 }
 
 module.exports = {
